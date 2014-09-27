53536542efedde1a5b31f95b008da1a3b2296673
Yunsheng Wei <weiyunshengwei@gmail.com>
2014-09-27 17:10:58 +0800

    Initialized PGM PA8 and PA9

---
+++ PGM-Programming_Assignment_8/ClassifyDataset.m
@@ -0,0 +1,23 @@
+function accuracy = ClassifyDataset(dataset, labels, P, G)
+% returns the accuracy of the model P and graph G on the dataset 
+%
+% Inputs:
+% dataset: N x 10 x 3, N test instances represented by 10 parts
+% labels:  N x 2 true class labels for the instances.
+%          labels(i,j)=1 if the ith instance belongs to class j 
+% P: struct array model parameters (explained in PA description)
+% G: graph structure and parameterization (explained in PA description) 
+%
+% Outputs:
+% accuracy: fraction of correctly classified instances (scalar)
+%
+% Copyright (C) Daphne Koller, Stanford Univerity, 2012
+
+N = size(dataset, 1);
+accuracy = 0.0;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% YOUR CODE HERE
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+fprintf('Accuracy: %.2f\n', accuracy);
